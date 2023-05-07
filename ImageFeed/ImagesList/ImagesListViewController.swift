437f8e2721846c018af874880b323cb9cda673d6
Alice <ad.digital@icloud.com>
2023-05-08 00:22:40 +0300

    sprint 08

---
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -0,0 +1,82 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Alice on 03.05.2023.
+//
+
+import UIKit
+
+class ImagesListViewController: UIViewController {
+    
+    @IBOutlet private var tableView: UITableView!
+    
+    private let photosName: [String] = Array(0..<20).map{ "\($0)" }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
+    }
+    
+    private lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        return formatter
+    }()
+}
+
+
+extension ImagesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return photosName.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
+        
+        guard let imageListCell = cell as? ImagesListCell else {
+            return UITableViewCell()
+        }
+        
+        configCell(for: imageListCell, with: indexPath)
+        
+        return imageListCell
+    }
+    
+    func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
+        cell.dataLabel.text = dateFormatter.string(from: Date())
+
+        let photoName = photosName[indexPath.row]
+        guard let image = UIImage(named: photoName) else { return }
+        cell.backgroundImageView.image = image
+        
+        if indexPath.row % 2 == 0 {
+            cell.likeButton.imageView?.image = UIImage(named: "Like")
+        } else {
+            cell.likeButton.imageView?.image = UIImage(named: "Unlike")
+        }
+    }
+}
+
+extension ImagesListViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        guard let image = UIImage(named: photosName[indexPath.row]) else {return 0}
+        
+        let imageWidth = image.size.width
+        let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
+        let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
+        let scale = imageViewWidth / imageWidth
+        let cellHeight = image.size.height * scale + imageInsets.top + imageInsets.bottom
+        return cellHeight
+    }
+}
+ 
