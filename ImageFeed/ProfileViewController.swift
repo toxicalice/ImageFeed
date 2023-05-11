e358d223377fd83aa801309f4b75d1536e7552ca
Alice <ad.digital@icloud.com>
2023-05-11 19:19:56 +0300

    add profile VC

---
+++ ImageFeed/ProfileViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Alice on 10.05.2023.
+//
+
+import Foundation
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    @IBOutlet var faceUIImage: UIImageView!
+    @IBOutlet var nameUILabel: UILabel!
+    @IBOutlet var nickNameLabel: UILabel!
+    @IBOutlet var textUILabel: UILabel!
+    @IBOutlet var exitButton: UIButton!
+    
+    @IBAction func onExitButtonClick(_ sender: UIButton) {
+    }
+    
+}
